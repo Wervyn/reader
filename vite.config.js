64722d53073c55c4af7c9bc0256261d5bd46c2f0
Wervyn <wervyn@gmail.com>
2026-07-31 17:09:22 -0400

    Site-relative path fix

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [vue()],
   server: {
     host: '0.0.0.0',
     port: 5173
   },
-  base: '/wervyn/honzuki/'
-})
+  base: mode === 'development' ? '/' : '/wervyn/honzuki/'
+}))
